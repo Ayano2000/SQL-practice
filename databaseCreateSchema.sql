b64ae385d4139816ad4327f97cf42eae50934201
Ayano2000 <aratayano00@gmail.com>
2021-03-25 19:44:41 +0200

    just pushing this up

--- databaseCreateSchema.sql
+++ databaseCreateSchema.sql
@@ -1,6 +1,4 @@
--- Table definitions
-
--- Create table to hold Applicant information
+-- Creating table for Applicants information
 CREATE table if not exists Applicants (
     ID INTEGER GENERATED ALWAYS AS IDENTITY,
     name varchar(64) not null,
@@ -21,9 +19,10 @@ CREATE table if not exists Applicants (
     PRIMARY KEY (ID)
 );
 
--- Create table to hold Questions for the Critical Thinking Test
-CREATE table if not exists Questions (
+-- Creating table to hold all of the Critical Thinking Test Questions
+CREATE table if not exists QuestionStore (
     ID INTEGER GENERATED ALWAYS AS IDENTITY,
+    active smallint not null,
     questionID int not null unique,
     question varchar(1024) not null,
     category varchar(64) not null,
@@ -35,19 +34,23 @@ CREATE table if not exists Questions (
     PRIMARY KEY (ID)
 );
 
--- Create table to hold the questions assigned to an Applicants test
+-- Create table to hold the 'test' ie. The questions that a test has been assigned
 CREATE table if not exists Tests (
     ID INTEGER GENERATED ALWAYS AS IDENTITY,
     testID varchar references Applicants(testID),
-    questionID int references Questions(questionID),
+    questionID int references QuestionStore(questionID),
     PRIMARY KEY (ID)
 );
 
--- todo - Create table to hold the answers the applicant submits
+-- Create table to hold the Applicants answers to the given test
+CREATE table if not exists  Answers (
+  ID INTEGER GENERATED ALWAYS AS IDENTITY,
+  testID varchar references Applicants(testID),
+  questionID int references QuestionStore(questionID),
+  answer char(1)
+);
 
--- Functions and their triggers
-
--- function to create the uid for an applicant
+-- Function to create the Applicants UID
 CREATE function create_uid() returns trigger AS $create_uid$
 DECLARE
     campus varchar(64);
@@ -74,13 +77,13 @@ BEGIN
 END;
 $create_uid$ LANGUAGE plpgsql;
 
--- trigger to call crate_uid function
+-- Trigger to call function to create applicant UID
 CREATE TRIGGER set_uid
     AFTER INSERT ON Applicants
     FOR EACH ROW EXECUTE FUNCTION create_uid();
 
 
--- function to create a testID for an applicant
+-- Function to create a test id for the applicant
 CREATE function create_testID() returns trigger AS $create_testID$
 DECLARE
     test_id varchar(64);
@@ -97,169 +100,22 @@ BEGIN
 END;
 $create_testID$ LANGUAGE plpgsql;
 
--- trigger to call the create testId function
+-- Trigger to call the function that creates the test id
 CREATE TRIGGER set_testID
     AFTER INSERT ON Applicants
     FOR EACH ROW EXECUTE FUNCTION create_testID();
 
--- Insert queries to populate the database a bit
+-- Rule to prevent deletion of any row on the QuestionStore table
+CREATE RULE "disable_deletes" AS
+    ON DELETE TO QuestionStore
+    DO INSTEAD NOTHING;
 
-INSERT INTO Applicants (name,
-                        surname,
-                        email,
-                        idNumber,
-                        cellphone,
-                        birthday,
-                        ethnicity,
-                        gender,
-                        campus,
-                        citizen,
-                        citizenship)
-                VALUES ('Arata',
-                        'Yano',
-                        'aratayano00@gmail.com',
-                        '0003235166083',
-                        '0762470205',
-                        '2000-03-23',
-                        'COLOURED',
-                        'MALE',
-                        '02',
-                        'y',
-                        'South Africa');
+-- Creating user for us to use when interacting with the db
+CREATE USER service_worker WITH PASSWORD 'password';
 
-INSERT INTO Applicants (name,
-                        surname,
-                        email,
-                        idNumber,
-                        cellphone,
-                        birthday,
-                        ethnicity,
-                        gender,
-                        campus,
-                        citizen,
-                        citizenship)
-VALUES ('Glen',
-        'Wasserfall',
-        'glen@wasserfall.com',
-        '9511125166834',
-        '0845168524',
-        '1995-11-12',
-        'WHITE',
-        'MALE',
-        '02',
-        'y',
-        'South Africa');
-
-INSERT INTO Applicants (name,
-                        surname,
-                        email,
-                        idNumber,
-                        cellphone,
-                        birthday,
-                        ethnicity,
-                        gender,
-                        campus,
-                        citizen,
-                        citizenship)
-VALUES ('Carah',
-        'Prinsloo',
-        'carahprinsloo@gmail.com',
-        '9905045166064',
-        '0842670205',
-        '1999-05-04',
-        'WHITE',
-        'FEMALE',
-        '02',
-        'y',
-        'South Africa');
-
-INSERT INTO Applicants (name,
-                        surname,
-                        email,
-                        idNumber,
-                        cellphone,
-                        birthday,
-                        ethnicity,
-                        gender,
-                        campus,
-                        citizen,
-                        citizenship)
-VALUES ('Rachel',
-        'Bolton',
-        'rachelericabolton@gmail.com',
-        '9602055164865',
-        '0635198754',
-        '1996-02-05',
-        'WHITE',
-        'FEMALE',
-        '02',
-        'y',
-        'South Africa');
-
-INSERT INTO Questions (questionID,
-                       category,
-                       question,
-                       optionA,
-                       optionB,
-                       optionC,
-                       optionD,
-                       answer)
-                VALUES ('1',
-                        'deductive reasoning',
-                        'What is 9 + 10?',
-                        '109',
-                        '19',
-                        '21',
-                        '910',
-                        'B');
-
-INSERT INTO Questions (questionID,
-                       category,
-                       question,
-                       optionA,
-                       optionB,
-                       optionC,
-                       optionD,
-                       answer)
-VALUES ('2',
-        'inductive reasoning',
-        'Where does the sun set?',
-        'In the North',
-        'In the South',
-        'In the West',
-        'In the East',
-        'C');
-
-INSERT INTO Questions (questionID,
-                       category,
-                       question,
-                       optionA,
-                       optionB,
-                       optionC,
-                       optionD,
-                       answer)
-VALUES ('3',
-        'arithmetic ability',
-        'What is the next number in the sequence: 2, 4, 7, 11, ..?',
-        '12',
-        '9',
-        '19',
-        '16',
-        'D');
-
-INSERT INTO Questions (questionID,
-                       category,
-                       question,
-                       optionA,
-                       optionB,
-                       optionC,
-                       optionD,
-                       answer)
-VALUES ('4',
-        'programming aptitude',
-        'If ADD = 9, BAD = 7 and CAD = 8 what is the value of ADA?',
-        '7',
-        '6',
-        '5',
-        '11',
-        'C');
+-- setting privileges for the user
+GRANT INSERT, SELECT, UPDATE ON Applicants to service_worker;
+GRANT INSERT, SELECT ON QuestionStore to service_worker;
+GRANT INSERT, SELECT ON Tests to service_worker;
+GRANT INSERT, SELECT ON Answers to service_worker;
+GRANT UPDATE (active) ON QuestionStore to service_worker;
--- startDb.sh
+++ startDb.sh
@@ -3,3 +3,6 @@ docker-compose up -d
 sleep 3
 export PGPASSWORD=postgres
 cat databaseCreateSchema.sql | psql -h localhost -U postgres -d applicationsite
+sleep 3
+export PGPASSWORD=password
+cat populateDatabase.sql | psql -h localhost -U service_worker -d applicationsite
